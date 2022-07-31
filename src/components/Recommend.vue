aa9e0836c7225cba9f2d041d38918f0a7af920c8
xuan <xuancbblin@gmail.com>
2022-07-31 23:30:30 +0800

    feat: 顧客推薦區塊切版完成

--- src/components/Recommend.vue
+++ src/components/Recommend.vue
@@ -4,69 +4,26 @@
       <h2 class="text-center text-c-red-700 mb-6 text-2xl md:mb-12 md:text-4xl">
         顧客推薦
       </h2>
-      <div class="md:flex flex-wrap">
+      <div class="md:flex md:flex-wrap md:-mx-2">
         <div
-          class="mb-5 shadow md:w-3/6 lg:w-1/4 md:box-border md:flex md:flex-col"
+          class="pb-6 md:w-3/6 md:px-4 lg:px-2 lg:w-1/4"
+          v-for="recommend in recommendList"
+          :key="recommend"
         >
-          <div>
-            <img
-              src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/week3-4/home-section5-1.png"
-              alt="home-section5-1"
-            />
-          </div>
-          <div class="py-3 px-3">
-            <h3 class="mb-3">Jessy</h3>
-            <p class="mb-20 md:mb-0">眼鏡品質優良，下次還會想來這邊購買！</p>
-            <time class="text-c-zinc-600">2021/06/20</time>
-          </div>
-        </div>
-        <div
-          class="mb-5 shadow md:w-3/6 lg:w-1/4 md:box-border md:flex md:flex-col"
-        >
-          <div>
-            <img
-              src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/week3-4/home-section5-2.png"
-              alt="home-section5-1"
-            />
-          </div>
-          <div class="py-3 px-3">
-            <h3 class="mb-3">凱倫</h3>
-            <p class="mb-20 md:mb-0">
-              做工細緻、鏡架很輕盈，待久也不會覺得有負擔，推薦給大家！
-            </p>
-            <time class="text-c-zinc-600">2021/04/18</time>
-          </div>
-        </div>
-        <div
-          class="mb-5 shadow md:w-3/6 lg:w-1/4 md:box-border md:flex md:flex-col"
-        >
-          <div>
-            <img
-              src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/week3-4/home-section5-3.png"
-              alt="home-section5-1"
-            />
-          </div>
-          <div class="py-3 px-3">
-            <h3 class="mb-3">悠悠</h3>
-            <p class="mb-20 md:mb-0">
-              謝謝客服人員的詳細回答，成功買到了喜歡的眼鏡，下次會再回購！
-            </p>
-            <time class="text-c-zinc-600">2020/12/25</time>
-          </div>
-        </div>
-        <div
-          class="mb-5 shadow md:w-3/6 lg:w-1/4 md:box-border md:flex md:flex-col"
-        >
-          <div>
-            <img
-              src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/week3-4/home-section5-4.png"
-              alt="home-section5-1"
-            />
-          </div>
-          <div class="py-3 px-3">
-            <h3 class="mb-3">Kyuan</h3>
-            <p class="mb-20 md:mb-0">服務很好，品質沒有任何問題，非常喜歡。</p>
-            <time class="text-c-zinc-600">2020/10/31</time>
+          <div class="shadow md:box-border md:flex md:flex-col md:h-full">
+            <div>
+              <img
+                :src="`https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/week3-4/${recommend.pic}`"
+                :alt="recommend.pic"
+              />
+            </div>
+            <div class="py-3 px-3 md:h-full md:flex md:flex-col">
+              <h3 class="mb-3">{{ recommend.title }}</h3>
+              <p class="mb-20">{{ recommend.desc }}</p>
+              <time class="text-c-zinc-600 md:mt-auto">{{
+                recommend.date
+              }}</time>
+            </div>
           </div>
         </div>
       </div>
@@ -74,6 +31,33 @@
   </section>
 </template>
 
-<script setup></script>
+<script setup>
+const recommendList = [
+  {
+    pic: "home-section5-1.png",
+    title: "Jessy",
+    desc: "眼鏡品質優良，下次還會想來這邊購買！",
+    date: "2021/06/20",
+  },
+  {
+    pic: "home-section5-2.png",
+    title: "凱倫",
+    desc: "做工細緻、鏡架很輕盈，待久也不會覺得有負擔，推薦給大家！",
+    date: "2021/04/18",
+  },
+  {
+    pic: "home-section5-3.png",
+    title: "悠悠",
+    desc: "  謝謝客服人員的詳細回答，成功買到了喜歡的眼鏡，下次會再回購！",
+    date: "2020/12/25",
+  },
+  {
+    pic: "home-section5-4.png",
+    title: "Kyuan",
+    desc: "服務很好，品質沒有任何問題，非常喜歡。",
+    date: "2020/10/31",
+  },
+];
+</script>
 
 <style lang="scss" scoped></style>
